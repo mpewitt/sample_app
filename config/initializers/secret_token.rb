3d08b54d56c9eac650c1f9f4c85bdfb4099bd40a
Michele Pewitt <mpewitt@getcix.com>
2011-12-13 13:28:39 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CixApp::Application.config.secret_token = 'b2bf06c5bf3ed5aa0f930c50a630e2163ba642c61f0b3cb0c4fa09115d832d348461e716aa3ff25a61825c0683f89881f3c5f8b75219bc73394e55c06ed40cc3'
